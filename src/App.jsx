0f19b77ca0eb003f228485aac451f3007351e4a5
Krithik Jain <krithikbaghmar@gmail.com>
2025-01-25 23:13:40 +0530

    CSS Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import './App.module.css'
+import styles from './App.module.css'
 
 function App() {
   return (
-    <>
+    <div className={styles.App}>
       Hello World!!
-    </>
+    </div>
   )
 }
 
