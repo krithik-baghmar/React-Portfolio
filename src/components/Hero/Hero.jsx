a8fc0867e45f3b3ad6fd9782a8f31023a3abc645
Krithik Jain <krithikbaghmar@gmail.com>
2025-01-27 03:34:15 +0530

    About Section Created

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import { getImageUrl } from "../../utils"
 const Hero = () => {
   return (
     <section className={styles.container}>
-      <div className={styles.contact}>
+      <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Krithik Jain</h1>
         <p className={styles.description}>I am a full-stack developer with 5 years of experience using React and NodeJs. Reach out if you'd like to learn more.</p>
         <a href="mailto:krithikbaghmar@gmail.com" className={styles.contactBtn}>Contact Me</a>
